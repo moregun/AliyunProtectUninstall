668960f5c4f1f512ee55b4146532bd1a505e007a
moregun <43885511+moregun@users.noreply.github.com>
2022-04-14 10:32:52 +0800

    Update TPU.sh
    新删除的TAT自动化助手

--- TPU.sh
+++ TPU.sh
@@ -22,6 +22,18 @@ kill 1274
 
 rm -rf /usr/local/qcloud
 
+bash /usr/local/qcloud/YunJing/uninst.sh
+
+bash /usr/local/qcloud/stargate/admin/uninstall.sh
+
+bash /usr/local/qcloud/monitor/barad/admin/uninstall.sh
+
+systemctl stop tat_agent
+
+systemctl disable tat_agent
+
+rm -f /etc/systemd/system/tat_agent.service
+
 ps -A | grep agent
 
 echo 执行成功!
